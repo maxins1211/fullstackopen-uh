c20ea287f2e9a96342eb98b0521bf74731eb57d9
Nghi Lam Vo <lamvo.general@gmail.com>
2025-04-03 12:10:29 -0400

    Add solution for exercise 3.3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -3,44 +3,50 @@ const app = express();
 
 const PORT = 3001;
 let phonebook = [
-    {
-        id: "1",
-        name: "Arto Hellas",
-        number: "040-123456",
-    },
-    {
-        id: "2",
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-    },
-    {
-        id: "3",
-        name: "Dan Abramov",
-        number: "12-43-234345",
-    },
-    {
-        id: "4",
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
-    },
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
 ];
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello from backend</h1>");
+  res.send("<h1>Hello from backend</h1>");
 });
 
 app.get("/api/persons", (req, res) => {
-    res.json(phonebook);
+  res.json(phonebook);
 });
 
 app.get("/info", (req, res) => {
-    const entryNumber = phonebook.length;
-    const currentDate = new Date();
-    const output = `<p>Phonebook has info for ${entryNumber} people 
+  const entryNumber = phonebook.length;
+  const currentDate = new Date();
+  const output = `<p>Phonebook has info for ${entryNumber} people 
     </p> <p>${currentDate}</p>`;
-    res.send(output);
+  res.send(output);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const person = phonebook.find((person) => person.id === id);
+  person ? res.json(person) : res.status(404).end();
 });
 
 app.listen(PORT, () => {
-    console.log("Server is running at port ", PORT);
+  console.log("Server is running at port ", PORT);
 });

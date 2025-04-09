395428f214ad09ee88dfa13e8bfe845f7ea8d787
Nghi Lam Vo <lamvo.general@gmail.com>
2025-04-08 22:35:28 -0400

    Add solution for exercise 3.18

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -4,28 +4,6 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const PORT = process.env.PORT;
-let phonebook = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
 
 app.use(express.static("dist"));
 app.use(express.json());
@@ -41,11 +19,12 @@ app.get("/api/persons", (req, res) => {
 });
 
 app.get("/info", (req, res) => {
-  const entryNumber = phonebook.length;
-  const currentDate = new Date();
-  const output = `<p>Phonebook has info for ${entryNumber} people 
-    </p> <p>${currentDate}</p>`;
-  res.send(output);
+  Contact.countDocuments().then((entryNumber) => {
+    const currentDate = new Date();
+    const output = `<p>Phonebook has info for ${entryNumber} people 
+      </p> <p>${currentDate}</p>`;
+    res.send(output);
+  });
 });
 
 app.get("/api/persons/:id", (req, res, next) => {

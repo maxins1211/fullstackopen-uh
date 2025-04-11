50586f6a0f60d7fa5617f0badda14dabda83fdd3
Nghi Lam Vo <lamvo.general@gmail.com>
2025-04-10 21:40:47 -0400

    Refactor and add solution for exercise 4.2

---
+++ part4/blog-list/app.js
@@ -0,0 +1,22 @@
+const config = require("./utils/config");
+const express = require("express");
+const morgan = require("morgan");
+const blogsRouter = require("./controllers/blogs.route");
+const mongoose = require("mongoose");
+const logger = require("./utils/logger");
+const app = express();
+
+mongoose
+    .connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info("Connected to MongoDB");
+    })
+    .catch((err) => {
+        logger.error("error connection to MongoDB:", err.message);
+    });
+morgan.token("req-body", (req) => JSON.stringify(req.body));
+app.use(express.json());
+app.use(morgan(":method :url :status - :response-time ms :req-body"));
+app.use("/api/blogs", blogsRouter);
+
+module.exports = app

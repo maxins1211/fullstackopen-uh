2522d9d997bf4ec1ce6b3b9586866136e2bb8aa8
Nghi Lam Vo <lamvo.general@gmail.com>
2025-04-21 21:50:27 -0400

    Add solution for exercise 4.10

--- part4/blog-list/tests/blogs_api.test.js
+++ part4/blog-list/tests/blogs_api.test.js
@@ -3,15 +3,15 @@ const { test, after, beforeEach } = require("node:test");
 const supertest = require("supertest");
 const assert = require("node:assert");
 const Blog = require("../models/blog");
-const helper = require("./test_helper")
+const helper = require("./test_helper");
 const app = require("../app");
 
 const api = supertest(app);
 
 beforeEach(async () => {
-    await Blog.deleteMany({})
-    await Blog.insertMany(helper.initialBlogs)
-})
+    await Blog.deleteMany({});
+    await Blog.insertMany(helper.initialBlogs);
+});
 
 test.only("all blogs are returned as JSON", async () => {
     await api
@@ -19,14 +19,33 @@ test.only("all blogs are returned as JSON", async () => {
         .expect(200)
         .expect("Content-Type", /application\/json/);
     const blogs = await helper.blogsInDb();
-    assert.strictEqual(blogs.length, helper.initialBlogs.length)
+    assert.strictEqual(blogs.length, helper.initialBlogs.length);
 });
 
 test.only("unique identifier property of blog post is named id", async () => {
     const blogs = await helper.blogsInDb();
-    assert(blogs[0].hasOwnProperty("id"), true)
-})
+    assert(blogs[0].hasOwnProperty("id"), true);
+});
 
+test.only("a valid blog can be added", async () => {
+    const blogsBeforeAdding = await helper.blogsInDb();
+    const newBlog = {
+        title: "Go To Statement Considered Harmful 2",
+        author: "Edsger W. Dijkstra",
+        url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+        likes: 6,
+    };
+    await api
+        .post("/api/blogs")
+        .send(newBlog)
+        .expect(201)
+        .expect("Content-Type", /application\/json/);
+
+    const blogsAfterAdding = await helper.blogsInDb();
+    assert.strictEqual(blogsBeforeAdding.length + 1, blogsAfterAdding.length)
+    const addedBlog = blogsAfterAdding[blogsAfterAdding.length - 1]
+    assert(addedBlog.title.includes("Go To Statement Considered Harmful 2"), true)
+});
 after(async () => {
-    await moogoose.connection.close()
-})
+    await moogoose.connection.close();
+});

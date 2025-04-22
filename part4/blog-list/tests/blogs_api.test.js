0c564a39770f3b0581bc9eb9d4c9a3a3448a197a
Nghi Lam Vo <lamvo.general@gmail.com>
2025-04-21 21:30:00 -0400

    Add solution for exercise 4.9

--- part4/blog-list/tests/blogs_api.test.js
+++ part4/blog-list/tests/blogs_api.test.js
@@ -3,30 +3,14 @@ const { test, after, beforeEach } = require("node:test");
 const supertest = require("supertest");
 const assert = require("node:assert");
 const Blog = require("../models/blog");
+const helper = require("./test_helper")
 const app = require("../app");
 
 const api = supertest(app);
 
-const initialBlogs = [{
-    _id: "5a422a851b54a676234d17f7",
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-    __v: 0
-},
-{
-    _id: "5a422aa71b54a676234d17f8",
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-    __v: 0
-},]
-
 beforeEach(async () => {
     await Blog.deleteMany({})
-    await Blog.insertMany(initialBlogs)
+    await Blog.insertMany(helper.initialBlogs)
 })
 
 test.only("all blogs are returned as JSON", async () => {
@@ -34,9 +18,14 @@ test.only("all blogs are returned as JSON", async () => {
         .get("/api/blogs")
         .expect(200)
         .expect("Content-Type", /application\/json/);
-    const blogs = await Blog.find({})
-    assert.strictEqual(blogs.length, initialBlogs.length)
+    const blogs = await helper.blogsInDb();
+    assert.strictEqual(blogs.length, helper.initialBlogs.length)
 });
+
+test.only("unique identifier property of blog post is named id", async () => {
+    const blogs = await helper.blogsInDb();
+    assert(blogs[0].hasOwnProperty("id"), true)
+})
 
 after(async () => {
     await moogoose.connection.close()

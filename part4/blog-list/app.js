1614899bec7011798844de1979a38916d885270a
Nghi Lam Vo <lamvo.general@gmail.com>
2025-04-22 22:49:43 -0400

    Add solution for exercise 4.15

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -3,6 +3,7 @@ const config = require("./utils/config");
 const express = require("express");
 const morgan = require("morgan");
 const blogsRouter = require("./controllers/blogs.route");
+const usersRouter = require("./controllers/users.route")
 const mongoose = require("mongoose");
 const logger = require("./utils/logger");
 const middleware = require("./utils/middleware")
@@ -20,6 +21,7 @@ morgan.token("req-body", (req) => JSON.stringify(req.body));
 app.use(express.json());
 app.use(morgan(":method :url :status - :response-time ms :req-body"));
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
---
+++ part4/blog-list/controllers/users.route.js
@@ -0,0 +1,29 @@
+const Blog = require("../models/blog");
+const User = require("../models/user")
+const usersRouter = require('express').Router()
+const bcrypt = require("bcryptjs")
+
+usersRouter.post('/', async (request, response) => {
+    const { username, name, password } = request.body
+
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+
+    const user = new User({
+        username,
+        name,
+        passwordHash,
+    })
+
+    const savedUser = await user.save()
+
+    response.status(201).json(savedUser)
+})
+
+usersRouter.get('/', async (request, response) => {
+    const users = await User.find({})
+    response.json(users)
+})
+
+
+module.exports = usersRouter

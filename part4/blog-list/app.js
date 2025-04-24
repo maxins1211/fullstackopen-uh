611d1ba0b6a5a4cc0e1b4ad0094e4cad56dd90d2
Nghi Lam Vo <lamvo.general@gmail.com>
2025-04-24 13:13:46 -0400

    Add solution for exercise 5.1

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -9,7 +9,7 @@ const mongoose = require("mongoose");
 const logger = require("./utils/logger");
 const middleware = require("./utils/middleware")
 const app = express();
-
+app.use(express.static('dist'))
 mongoose
     .connect(config.MONGODB_URI)
     .then(() => {
@@ -21,8 +21,8 @@ mongoose
 morgan.token("req-body", (req) => JSON.stringify(req.body));
 app.use(express.json());
 app.use(morgan(":method :url status: :status - :response-time ms :req-body"));
-app.use(middleware.tokenExtractor)
 app.use("/api/login", loginRouter)
+app.use(middleware.tokenExtractor)
 app.use("/api/blogs", middleware.userExtractor, blogsRouter);
 app.use("/api/users", usersRouter)
 app.use(middleware.unknownEndpoint)

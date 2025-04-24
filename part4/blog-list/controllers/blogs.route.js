40c5fc6539e0863ab8fcf2ca725bd1333895f8ef
Nghi Lam Vo <lamvo.general@gmail.com>
2025-04-23 23:29:41 -0400

    Add solution for exercise 4.22

--- part4/blog-list/controllers/blogs.route.js
+++ part4/blog-list/controllers/blogs.route.js
@@ -9,12 +9,8 @@ blogsRouter.get("/", async (request, response) => {
 });
 
 blogsRouter.post("/", async (request, response) => {
-    const body = request.body;
-    const decodedToken = jwt.verify(request.token, process.env.SECRET)
-    if (!decodedToken.id) {
-        return response.status(401).json({ error: "Token invalid" })
-    }
-    const user = await User.findById(decodedToken.id)
+    const { body, user } = request;
+    console.log(user)
     const blog = new Blog({
         title: body.title,
         author: body.author,
@@ -29,12 +25,7 @@ blogsRouter.post("/", async (request, response) => {
 });
 
 blogsRouter.delete("/:id", async (request, response) => {
-    const token = request.token;
-    const decodedToken = jwt.verify(token, process.env.SECRET)
-    if (!decodedToken.id) {
-        return response.status(401).json({ error: "Token invalid" })
-    }
-    const user = await User.findById(decodedToken.id);
+    const user = request.user;
     const id = request.params.id
     const blog = await Blog.findById(id);
     if (blog.user.toString() === user._id.toString()) {

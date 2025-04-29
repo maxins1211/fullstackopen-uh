dc8fc1074f24debe41b0097b7007b8ceba1a7fa2
Nghi Lam Vo <lamvo.general@gmail.com>
2025-04-29 12:28:26 -0400

    Add solution for exercise 5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,21 +1,36 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
+import { beforeEach, describe } from 'vitest'
 
-test('renders only title and author', () => {
-  const blog = {
-    title: 'not a real title',
-    url: 'not a real url',
-    author: 'not a real author',
-    likes: 6,
-    user: { name: 'Lam' },
-  }
-  const currentUser = {
-    name: 'Lam',
-  }
-  const { container } = render(<Blog blog={blog} currentUser={currentUser} />)
-  screen.debug()
-  const element = container.querySelector('.blog-header')
-  expect(element).toHaveTextContent('not a real title not a real author view')
-  const body = container.querySelector('.blog-body')
-  expect(body).not.toHaveStyle('display: block')
+describe('<Blog />', () => {
+  let container
+  beforeEach(() => {
+    const blog = {
+      title: 'not a real title',
+      url: 'not a real url',
+      author: 'not a real author',
+      likes: 6,
+      user: { name: 'Lam' },
+    }
+    const currentUser = {
+      name: 'Lam',
+    }
+    container = render(<Blog blog={blog} currentUser={currentUser} />).container
+  })
+
+  test('initially renders only title and author', () => {
+    const element = container.querySelector('.blog-header')
+    expect(element).toHaveTextContent('not a real title not a real author view')
+    const body = container.querySelector('.blog-body')
+    expect(body).toHaveStyle('display: none')
+  })
+
+  test('after clicking the view button, the blog detail is displayed', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+    const body = container.querySelector('.blog-body')
+    expect(body).not.toHaveStyle('display: none')
+  })
 })

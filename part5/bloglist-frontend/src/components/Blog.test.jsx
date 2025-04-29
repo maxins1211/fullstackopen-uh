50d81ee9a8c3eaafa7917f7ca68bce0014a3f80c
Nghi Lam Vo <lamvo.general@gmail.com>
2025-04-29 12:33:28 -0400

    Add solution for exercise 5.15

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -5,6 +5,7 @@ import { beforeEach, describe } from 'vitest'
 
 describe('<Blog />', () => {
   let container
+  const mockHandler = vi.fn()
   beforeEach(() => {
     const blog = {
       title: 'not a real title',
@@ -16,7 +17,9 @@ describe('<Blog />', () => {
     const currentUser = {
       name: 'Lam',
     }
-    container = render(<Blog blog={blog} currentUser={currentUser} />).container
+    container = render(
+      <Blog blog={blog} currentUser={currentUser} increaseLike={mockHandler} />,
+    ).container
   })
 
   test('initially renders only title and author', () => {
@@ -32,5 +35,15 @@ describe('<Blog />', () => {
     await user.click(button)
     const body = container.querySelector('.blog-body')
     expect(body).not.toHaveStyle('display: none')
+  })
+
+  test('when the button is clicked twice, the event handler is called twice', async () => {
+    const user = userEvent.setup()
+    const viewButton = screen.getByText('view')
+    await user.click(viewButton)
+    const likeButton = screen.getByText('like')
+    await user.click(likeButton)
+    await user.click(likeButton)
+    expect(mockHandler.mock.calls).toHaveLength(2)
   })
 })

9cb8611ea8da005ac64b01e99d06a91523df3ab9
Nghi Lam Vo <lamvo.general@gmail.com>
2025-04-29 21:43:53 -0400

    Add solution for exercise 5.18

--- part5/bloglist-e2e-testing/tests/blog_app.spec.js
+++ part5/bloglist-e2e-testing/tests/blog_app.spec.js
@@ -3,7 +3,15 @@ const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('Blog app', () => {
 
-    beforeEach(async ({ page }) => {
+    beforeEach(async ({ page, request }) => {
+        await request.post('http://localhost:3003/api/testing/reset')
+        await request.post('http://localhost:3003/api/users', {
+            data: {
+                name: 'lam test',
+                username: 'blogtest',
+                password: '12345'
+            }
+        })
         await page.goto('http://localhost:5173')
     })
     test('displays the login form by default', async ({ page }) => {
@@ -14,4 +22,24 @@ describe('Blog app', () => {
         const passwordInput = page.getByTestId('password')
         await expect(passwordInput).toBeVisible()
     })
+
+    describe('Login', () => {
+        test('succeeds with correct credentials', async ({ page }) => {
+            await page.getByTestId('username').fill('blogtest')
+            await page.getByTestId('password').fill('12345')
+            await page.getByRole('button', { name: 'login' }).click()
+            const messageDiv = await page.locator('.message')
+            await expect(messageDiv).toContainText('Login successfully')
+        })
+
+        test('fails with wrong credentials', async ({ page }) => {
+            await page.getByTestId('username').fill('blogtest')
+            await page.getByTestId('password').fill('wrong')
+            await page.getByRole('button', { name: 'login' }).click()
+            const messageDiv = await page.locator('.error')
+            await expect(messageDiv).toContainText('Wrong username or password')
+        })
+    })
+
+
 })
